9c0eac7e4088bbf1dab63b773b412639fd9af8fc
Ilsan2002 <93679633+Ilsan2002@users.noreply.github.com>
2021-11-07 17:30:22 -0500

    Add files via upload

---
+++ A = [1,2,3,4,5].py
@@ -0,0 +1,11 @@
+A = [1,2,3,4,5]
+B = 3
+C = 69
+def reoreo(A,B,C):
+    A = [1,2,3,4,5]
+    B = 3
+    C = 69
+    for i in range(len(A)):
+        if A[i] == B:
+            A[i] = C
+print(A)            
